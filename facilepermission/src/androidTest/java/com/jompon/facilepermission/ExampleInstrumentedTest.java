d0e57a611a2f4988ddbe344852b077b1188f5756
jompons <snookerace6211@gmail.com>
2018-02-27 15:17:23 +0700

    Create Lib Module

--- facilepermission/src/androidTest/java/com/jompon/facilepermission/ExampleInstrumentedTest.java
+++ facilepermission/src/androidTest/java/com/jompon/facilepermission/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.jompon.facilepermission", appContext.getPackageName());
+        assertEquals("com.jompon.facilepermission.test", appContext.getPackageName());
     }
 }
